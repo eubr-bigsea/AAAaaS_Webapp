6f8e6ac9c42bf8b63f183128eb0a58495baff0a7
Paulo Silva <paulo308@users.noreply.github.com>
2017-07-16 18:37:33 +0100

    Update basedb.py

--- aaa_manager/basedb.py
+++ aaa_manager/basedb.py
@@ -10,11 +10,12 @@ from aaa_manager.db_client import DBClient
 LOG = logging.getLogger(__name__)
 
 #_DEFAULT_DB_HOST = "AUTH_DB_HOST" in os.environ
-_DEFAULT_DB_HOST = os.getenv("AUTH_DB_HOST", "mongo")
+#_DEFAULT_DB_HOST = os.getenv("AUTH_DB_HOST", "mongo")
 #_DEFAULT_DB_PORT = "AUTH_DB_PORT" in os.environ
-_DEFAULT_DB_PORT = int(os.getenv("AUTH_DB_PORT", 27017))
+#_DEFAULT_DB_PORT = int(os.getenv("AUTH_DB_PORT", 27017))
 _DEFAULT_DB_NAME = 'AAADB'
-
+_DEFAULT_DB_HOST = '10.0.0.7'
+_DEFAULT_DB_PORT = 44802
 
 
 
